86b6555362c07b020eaa0a1b33d1039fc0638040
emilianosal <emilianosal@hotmail.com>
2023-09-18 17:02:07 +0200

    pre test

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer()
+
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.use(function middleware(req, res, next){
+  console.log(req.method+" "+req.path+" - "+req.ip)
+  next();
+})
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
